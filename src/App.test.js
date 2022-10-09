40f8191c48013f28bb1612133cac63de6faa301a
Charles Heller <charles.heller@hotmail.de>
2022-10-10 01:02:31 +0200

    modified yml file again

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/This Is My First Step To A Big Dream!!!/i);
+  const linkElement = screen.getByText(/Connection to Hostinger was success !!!/i);
   expect(linkElement).toBeInTheDocument();
 });
