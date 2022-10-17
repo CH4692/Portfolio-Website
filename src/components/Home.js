8c10c70642ba4bf69fb24fdeafd4617946097206
Charles Heller <charles.heller@hotmail.de>
2022-10-17 12:58:45 +0200

    added first Content on the Homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,29 @@
-import { Typography } from '@mui/material'
+import { Button, styled, Typography } from '@mui/material'
+import { Box } from '@mui/system'
 import React from 'react'
 
+const StyledButton = styled(Button)({
+  width: '300px',
+  height: '50px',
+  borderRadius: '5px',
+  color: 'primary.dark'
+
+})
 const Home = () => {
   return (
-    <Typography variant='p'>You are on the Home Page</Typography>
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', height: '100%', width: '100%', pl: '7em', pt: '7em' }}>
+      <Typography variant='h6' color={'primary.dark'} fontWeight={'bold'}>Hi, my name is.</Typography>
+      <Typography variant='h2' color='primary.light' fontWeight={'bold'}>Charles Heller</Typography>
+      <Typography variant='h2' color={'primary.dark'} mb='20px' fontWeight={'bold'}>Frontend-Backend Developer</Typography>
+      <Typography variant='p' mb='50px' color={'primary.dark'}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elit, sed do<br />
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim Ut enim<br />
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut  nisi ut<br />
+        aliquip ex ea commodo consequat.
+      </Typography>
+      <StyledButton size='large' variant="outlined" >Check out my Projects!</StyledButton>
+
+    </Box>
   )
 }
 
--- src/components/Layout/Feed.js
+++ src/components/Layout/Feed.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Feed = (props) => {
   return (
-  <Box bgcolor='green' flex={6} p={2}>
+  <Box flex={6} p={2}>
     {props.children}
   </Box>
   )
